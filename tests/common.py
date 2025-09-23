1f74d603cf53f86a32cc2347e61f29ba83a7d921
ZYX223 <zhangyuxinxhu@163.com>
2025-09-23 10:14:22 +0800

    tett02

--- tests/common.py
+++ tests/common.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import pathlib
-from functools import lru_cache
+from functools import lru_cache 
 
 FIXTURES_PATH = (pathlib.Path(__file__).resolve().parent) / "fixtures"
 
