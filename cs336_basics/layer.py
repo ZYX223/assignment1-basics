df26feeebe0460c5928be8d993363526646718cf
ZYX223 <957851256@qq.com>
2025-10-13 21:52:32 +0800

    add embedding

--- cs336_basics/layer.py
+++ cs336_basics/layer.py
@@ -18,3 +18,20 @@ class Linear(nn.Module):
 
     def forward(self, x) :
         return einsum(x , self.weight , "... d_in, d_out d_in -> ... d_out")
+    
+class Embedding(nn.Module):
+    def __init__(self, vocab_size: int, d_model: int, device: None, dtype: None) :
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        factory_kwargs = {'device': device, 'dtype': dtype}
+
+        # Weights of shape (vocab_size, d_model)
+        self.weight = nn.Parameter(torch.empty((vocab_size, d_model), **factory_kwargs))
+        # Initialize weights using truncated normal
+        std = 1
+        init.trunc_normal_(self.weight, mean=0.0, std=std, a=-3*std, b=3*std)
+
+
+    def forward(self, token_ids) :
+        return self.weight[token_ids]
